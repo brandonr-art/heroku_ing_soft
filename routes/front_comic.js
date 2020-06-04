31992b557851b34ccf9cf6dfa95d6cd466503248
brandonr-art <brandoncelso1@gmail.com>
2020-06-04 16:05:00 -0500

    Se inicializa el proyecto

---
+++ routes/front_comic.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+router.get('/listar',(req,res,next) => {
+  request.get('http://localhost:3000/marvel/api/comic', (err,response,body)=>{
+    if(err) res.status(404).json({mensaje:"Error al consumir get comic"});
+    else res.render('comic_view',{'datos': JSON.parse(body) });
+  });
+});
+
+module.exports = router;
