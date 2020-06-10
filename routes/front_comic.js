b82fc2076058c0ea2dbaf76fc151c50257a33143
brandonr-art <brandoncelso1@gmail.com>
2020-06-10 17:42:17 -0500

    Ruta en Heroku

--- routes/front_comic.js
+++ routes/front_comic.js
@@ -6,7 +6,7 @@ var request = require('request');
 
 
 router.get('/listar',(req,res,next) => {
-  request.get('http://localhost:3000/marvel/api/comic' || 'https://ingsoftbrp.herokuapp.com/marvel/api/comic' , (err,response,body)=>{
+  request.get('https://ingsoftbrp.herokuapp.com/marvel/api/comic' , (err,response,body)=>{
     if(err) res.status(404).json({mensaje:"Error al consumir get comic"});
     else res.render('comic_view',{'datos': JSON.parse(body) });
   });
