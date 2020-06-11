e4299e0ab8e6bae55227d604027a41452f77ad79
brandonr-art <brandoncelso1@gmail.com>
2020-06-10 19:32:19 -0500

    URL Variable

--- routes/front_comic.js
+++ routes/front_comic.js
@@ -2,12 +2,17 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+const app  = express();
+app.set('port', process.env.PORT || '3000');
+
 
 
 
 router.get('/listar',(req,res,next) => {
-  request.get('https://ingsoftbrp.herokuapp.com/marvel/api/comic' , (err,response,body)=>{
-    if(err) res.status(404).json({mensaje:"Error al consumir get comic"});
+  //var url = req.host + ':'+app.get('port') + '/marvel/api/comic'
+  var url = req.protocol + '://' + req.hostname +'/marvel/api/comic'
+  request.get(url , (err,response,body)=>{
+    if(err) res.status(404).json({mensaje:url});
     else res.render('comic_view',{'datos': JSON.parse(body) });
   });
 });
