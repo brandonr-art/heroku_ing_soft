3203b9f48e7bc0a00fc6274a5e3ba6ca5593913a
brandonr-art <brandoncelso1@gmail.com>
2020-06-10 20:09:29 -0500

    URL absolut

--- routes/front_comic.js
+++ routes/front_comic.js
@@ -7,14 +7,24 @@ app.set('port', process.env.PORT || '3000');
 
 
 
-
+//var url = req.protocol + '://' + req.hostname +'/marvel/api/comic'
 router.get('/listar',(req,res,next) => {
-  //var url = req.host + ':'+app.get('port') + '/marvel/api/comic'
   var url = req.protocol + '://' + req.hostname +'/marvel/api/comic'
-  request.get(url , (err,response,body)=>{
-    if(err) res.status(404).json({mensaje:url});
-    else res.render('comic_view',{'datos': JSON.parse(body) });
+  var urlLocal = req.protocol + '://' + req.hostname + ":" + app.get('port') + '/marvel/api/comic'
+  
+  request.get(urlLocal , (err,response,body)=>{
+    if(err){
+      request.get(url , (err,response,body)=>{
+      if(err) res.status(404).json({mensaje:url});
+      else res.render('comic_view',{'datos': JSON.parse(body) });
+    });
+
+    }else res.render('comic_view',{'datos': JSON.parse(body) });
   });
+  
 });
+
+
+
 
 module.exports = router;
